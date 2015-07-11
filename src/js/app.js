315aef447190fed3cf6590a113c3d19da2051253
Aday Talavera <aday.talavera@gmail.com>
2015-07-11 11:19:41 +0100

    Refactoring loading resources with ajax

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,26 @@
+var resources = {
+	get: function(){
+		var json = null;
+		$.ajax({
+		    'async': false,
+		    'global': false,
+		    'url': 'resources.json',
+		    'dataType': "json",
+		    'success': function (data) {
+		        json = data;
+		    }
+		});
+		return json;
+	},
+	render: function(json) {
+		var links = $('#links');
+		json.forEach(function(data) {
+			links.append('<a href="' + data.url + '" class="list-group-item"><h4 class="list-group-item-heading">' + data.title + '</h4></a>');
+		});
+	}
+};
+
 jQuery(function($){
-	var json = (function () {
-	    var json = null;
-	    $.ajax({
-	        'async': false,
-	        'global': false,
-	        'url': 'resources.json',
-	        'dataType': "json",
-	        'success': function (data) {
-	            json = data;	            
-
-	            var links = $('#links');
-
-	            json.forEach(function(data) {
-	            	links.append('<a href="' + data.url + '" class="list-group-item"><h4 class="list-group-item-heading">' + data.title + '</h4></a>');
-	            });
-	        }
-	    });
-	    return json;
-	})();	
+	var json = resources.get();
+	resources.render(json);
 });
